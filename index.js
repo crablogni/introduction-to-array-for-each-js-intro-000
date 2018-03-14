663c85adbce896035837bc32e08882db59719372
hector ariceaga <hector_ariceaga@hotmail.com>
2018-03-14 18:11:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ function doToElementsInArray(array, callback){
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array){
   array[index] = `How about ${index} in a string?`;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> cdd3e454d5efaa10020a3643331919368a29088d
